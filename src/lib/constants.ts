16e71f0b55cb9317735157fd5f816521cabefd27
Sn0w123 <lucasskold49@gmail.com>
2026-09-20 01:19:13 +0200

    Enable keyword transitions for supported browsers

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,4 +2,4 @@ export const BORDER_GRADIENT =
     "relative after:absolute after:top-0 after:right-0 after:h-full after:w-px after:bg-gradient-to-t after:from-border after:via-border/28 after:to-transparent";
 
 export const REPO_TOOLBAR_TRIGGER_CLASS =
-    "flex h-full min-w-32 lg:min-w-64 cursor-pointer items-center justify-between border-b px-4 py-2 hover:bg-accent";
+    "flex h-full w-auto transition-[width] ease-snappy min-w-32 lg:min-w-64 cursor-pointer items-center justify-between border-b px-4 py-2 hover:bg-accent";
